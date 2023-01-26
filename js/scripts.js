7b9c0d74e7b0aa0923360693d66e116024f2a13f
Owen McGrath <owenmcgrath85@gmail.com>
2023-01-26 18:28:55 -0500

    Images for attractions

--- js/scripts.js
+++ js/scripts.js
@@ -33,12 +33,12 @@ $(document).ready(function () {
         offset: '75%'
     });
     $('.wp7').waypoint(function () {
-        $('.wp7').addClass('animated fadeInUp');
+        $('.wp7').addClass('animated fadeInLeft');
     }, {
         offset: '75%'
     });
     $('.wp8').waypoint(function () {
-        $('.wp8').addClass('animated fadeInLeft');
+        $('.wp8').addClass('animated fadeInRight');
     }, {
         offset: '75%'
     });
